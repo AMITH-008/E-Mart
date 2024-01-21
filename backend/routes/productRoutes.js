531397c7135502ad2a8120f549f8799e9f061cc0
amith <amithkouti@gmail.com>
2024-01-21 08:26:26 +0530

    Added errorMiddleware and updated productRoutes

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,17 +1,23 @@
 import express from 'express';
-import products from '../data/products.js';
+import asyncHandler from '../middleware/asyncHandler.js';
 import Product from '../models/productModel.js';
 
 const router = express.Router();
 
 
-router.get('/', async (request, response) => {
-    const productsList = await Product.find();
+router.get('/', asyncHandler(async (request, response) => {
+    const products = await Product.find();
     response.json(products);
-});
+}));
 
-router.get('/:productId', (request, response) => {
-    response.json(products.find(product => product._id === request.params.productId));
-});
+router.get('/:productId', asyncHandler( async (request, response) => {
+    const product = await Product.findById(request.params.productId);
+    if(product)
+    {
+        return response.json(product);
+    }
+
+    response.statue(404).json({message: "Product Not found"});
+}));
 
 export default router;
